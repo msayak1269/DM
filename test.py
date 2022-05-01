f01c13ced252cdbbb1293904c31c0dd951f99673
msayak1269 <msayak1269@gmail.com>
2022-05-01 16:42:52 +0530

    second

--- test.py
+++
@@ -1,9 +0,0 @@
-def getResult():
-    # input = request.form.to_dict()
-    attributes = [[0, 135,  68, 42, 250,  42.3, 0.365,  24]] 
-    col = ['Pregnancies', 'Glucose',  'BloodPressure',  'SkinThickness',  'Insulin',  'BMI',  'DiabetesPedigreeFunction', 'Age']
-    df1 = pd.DataFrame(attributes, columns = col)
-    loaded_model = pickle.load(open("RF_model.pkl", "rb"))
-    result = loaded_model.predict(df1)
-    print(result[0])
-
